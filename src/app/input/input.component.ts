fe67142b97214a9673a3a732489712dd2efb2d60
Anas <anas.kala@hotmail.com>
2023-12-10 21:02:46 +0100

    adding some styling

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -8,5 +8,11 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent {
   @Input() control!: FormControl;
-  
+  @Input() label: string = '';
+
+  public showErrors() {
+    // the following line means, look at the properties of this control object
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
